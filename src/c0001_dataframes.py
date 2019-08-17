03ff3069ff11fa4ff04290b70e4d47191e72d720
krishna.shreedhar <krishna.shreedhar@gmail.com>
2019-08-17 22:28:09 +0530

    Add create_dataframe_from_dict_of_dict
    Create a Data Frame from Dict of Dict

--- src/c0001_dataframes.py
+++ src/c0001_dataframes.py
@@ -13,9 +13,19 @@ def create_dataframe_from_dict():
     return df
 
 
+def create_dataframe_from_dict_of_dict():
+    dict_b = {'col1': {'row1': 10, 'row2': 20, 'row3': 30, 'row4': 40, 'row5': 50},
+              'col2': {'row1': 'a', 'row2': 'b', 'row3': 'c', 'row4': 'd', 'row5': 'e'},
+              'col3': {'row1': '!', 'row2': '@', 'row3': '#', 'row4': '$', 'row5': '%'}}
+    df = pd.DataFrame(dict_b)
+    return df
+
+
 def main():
     df = create_dataframe_from_dict()
     print_df(df)
+    df1 = create_dataframe_from_dict_of_dict()
+    print(df1)
 
 
 if __name__ == '__main__':
