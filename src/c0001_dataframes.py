bd3942e37a81d63e846c1d6b5d0122d31fde0f23
krishna.shreedhar <krishna.shreedhar@gmail.com>
2019-08-19 22:39:50 +0530

    Add sort_values and reset_index

--- src/c0001_dataframes.py
+++ src/c0001_dataframes.py
@@ -24,7 +24,9 @@ def create_dataframe_from_dict() -> pd.DataFrame:
     Creates a dummy DataFrame from a basic python dictionary of lists.
     :return:
     """
-    dict_a = {'col1': [10, 20, 30, 40, 50], 'col2': ['a', 'b', 'c', 'd', 'e'], 'col3': ['!', '@', '#', '$', '%']}
+    dict_a = {'col1': [10, 20, 30, 40, 50, 60],
+              'col2': ['a', 'b', 'c', 'd', 'e', 'a'],
+              'col3': ['!', '@', '#', '$', '%', '#']}
     df = pd.DataFrame(dict_a)
     return df
 
@@ -41,6 +43,42 @@ def create_dataframe_from_dict_of_dict() -> pd.DataFrame:
     return df
 
 
+def sort_df(df, list_columns, list_bool_ascending):
+    """
+    Sort the DataFrame using list of columns provided.
+    :param df:
+    :param list_columns:
+    :param list_bool_ascending:
+    :return:
+
+    """
+    # Make a copy of DataFrame to avoid any mis-references.
+    df_tmp = df.copy()
+    df_tmp.sort_values(by=list_columns, ascending=list_bool_ascending, inplace=True)
+
+    # Other way
+    # df_tmp = df.sort_values(by=list_columns, ascending=list_bool_ascending)
+    return df_tmp
+
+
+def df_reset_index(df):
+    """
+    Code to reset the index i.e. row numbers
+    :param df:
+    :return:
+    """
+    # Don't assign to any variable because reset_index does not return the DataFrame.
+    #  NoneType / AttributeError will be raised.
+    df.reset_index(drop=True, inplace=True)
+    return df
+
+
+def invocations():
+    # df1 = create_dataframe_from_dict_of_dict()
+    # print_df(df1)
+    pass
+
+
 def main():
     """
 
@@ -48,8 +86,10 @@ def main():
     """
     df = create_dataframe_from_dict()
     print_df(df)
-    df1 = create_dataframe_from_dict_of_dict()
-    print_df(df1)
+    df = sort_df(df, ['col2', 'col1'], [False, True])
+    print_df(df)
+    df = df_reset_index(df)
+    print_df(df)
 
 
 if __name__ == '__main__':
