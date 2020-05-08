9d195905692e6aa292c3cd6bd55895d87b574334
shreedhar <krishna.shreedhar@gmail.com>
2020-05-08 12:16:32 +0530

    Add code for removing punctuation

--- src/nlp/data_preparation.py
+++ src/nlp/data_preparation.py
@@ -1,20 +1,36 @@
+import string
 import re
 
 
+def remove_non_printable_from_words(list_words):
+    re_print = re.compile("[^%s]" % re.escape(string.printable))
+    list_printable = [re_print.sub('', w) for w in list_words]
+    return list_printable
+
+
+def remove_punctuation_from_words(list_words):
+    print(f'removing: {string.punctuation}')
+    # prepare regex for char filtering
+    re_punc = re.compile('[%s]' % re.escape(string.punctuation))
+    # remove punctuation from each word
+    list_stripped = [re_punc.sub('', w) for w in list_words]
+    return list_stripped
+
+
 def splitting_into_words(text, **kwargs):
-    words = []
+    list_words = []
     splitter = kwargs.get('splitter', 're')
     re_expression = kwargs.get('re_expression', r'\W+')
 
     if splitter == 're':
-        words = re.split(re_expression, text)
+        list_words = re.split(re_expression, text)
     elif splitter == 'whitespaces':
-        words = text.split()
+        list_words = text.split()
     else:
         # Return sentences
-        words = text.replace('\n', ' ').split('.')
+        list_words = text.replace('\n', ' ').split('.')
 
-    return words
+    return list_words
 
 
 def load_text():
@@ -27,9 +43,11 @@ def load_text():
 
 def preprocess_initial():
     text = load_text()
-    words = splitting_into_words(text)
-    words = splitting_into_words(text, splitter='')
-    print(words[:100])
+    list_words = splitting_into_words(text)
+    list_words = splitting_into_words(text, splitter='')
+    list_words = remove_punctuation_from_words(list_words)
+    list_words = remove_non_printable_from_words(list_words)
+    print(list_words[:100])
 
 
 def main():
