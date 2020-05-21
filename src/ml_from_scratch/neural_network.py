708d554db55fa5b037c197ed7359afac9163dbdf
shreedhar <krishna.shreedhar@gmail.com>
2020-05-21 12:06:11 +0530

    Add function placeholders
    - Add to gitignore
    - Add references to the tutorial

--- src/ml_from_scratch/neural_network.py
+++ src/ml_from_scratch/neural_network.py
@@ -1,4 +1,39 @@
 """
-This module will implement neural network from scratch with Back Propagation
+This module will implement neural network from scratch with Back Propagation:
+1. Forward-propagation to calculate the error/loss
+2. Gradient
+3. Back propagation to update the weights
+
+Modelling as Classification problem:
+1. Tutorial link: https://machinelearningmastery.com/implement-backpropagation-algorithm-scratch-python/
+2. Dataset link: http://archive.ics.uci.edu/ml/datasets/seeds
+
+More resources:
+1. http://www.wildml.com/2015/09/implementing-a-neural-network-from-scratch/
+2. https://towardsdatascience.com/how-to-build-your-own-neural-network-from-scratch-in-python-68998a08e4f6
+
 """
 
+
+def initialize_network():
+    pass
+
+
+def forward_propagate():
+    pass
+
+
+def back_propagate_error():
+    pass
+
+
+def train_network():
+    pass
+
+
+def predict():
+    pass
+
+
+def case_study():
+    pass
