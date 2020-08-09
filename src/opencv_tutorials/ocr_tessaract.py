9b7b0ee35baaaf03a37114bb9875e64ce2809f1f
shreedhar <krishna.shreedhar@gmail.com>
2020-08-09 16:20:01 +0530

    add some code

---
+++ src/opencv_tutorials/ocr_tessaract.py
@@ -0,0 +1,12 @@
+"""
+
+"""
+
+import cv2
+import pytesseract
+
+img = cv2.imread('data/image.jpg')
+
+# Adding custom options
+custom_config = r'--oem 3 --psm 6'
+pytesseract.image_to_string(img, config=custom_config)
